4aba07a7271371d7fee70f9de7eaa2be5284eae6
Niveditha Aravind.V <107673750+NIVEDITHANIHA@users.noreply.github.com>
2022-08-06 14:20:33 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,13 +1,15 @@
+//displaying input
 function buttonClick(val) {
     document.getElementById("screen").value =  document.getElementById("screen").value +val;
 
     
 }
+//clear result
 function clearDisplay( ) {
     document.getElementById("screen").value = " "
     
 }
-
+//solving
 function equalClick( ) {
     var text =  document.getElementById("screen").value 
     var result = eval(text)
